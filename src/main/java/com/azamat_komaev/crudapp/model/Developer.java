8f362d4119a5af542f0f33d4b78e5351ae58341d
Азамат Комаев <azamatkomaev15@gmail.com>
2022-12-20 17:30:35 +0300

    feature(Developer.java): add Developer entity

---
+++ src/main/java/com/azamat_komaev/crudapp/model/Developer.java
@@ -0,0 +1,51 @@
+package com.azamat_komaev.crudapp.model;
+
+import java.util.List;
+
+public class Developer {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private List<Skill> skills;
+    private Specialty specialty;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+
+    public Specialty getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(Specialty specialty) {
+        this.specialty = specialty;
+    }
+}
